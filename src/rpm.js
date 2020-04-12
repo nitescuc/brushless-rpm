445829086ab6ed4863ceeee663dd5e5054044662
NITESCU Cristian <cristian.nitescu@renault.com>
2020-04-12 10:54:40 +0200

    Added kalman filter

--- src/rpm.js
+++ src/rpm.js
@@ -1,8 +1,16 @@
 const Gpio = require('pigpio').Gpio;
+const KalmanFilter = require('kalmanjs');
 
 class RpmReader {
     constructor(config) {
         this.config = config || {};
+        this.config.minValue = this.config.minValue || 0;
+        this.config.maxValue = this.config.maxValue || 10000;
+        this.config.remapValues = this.config.remapValues || [0, 10000];
+
+        if (this.config.useKalman) {
+            this.kalmanFilter = new KalmanFilter();
+        }
 
         this.inputPin = new Gpio(this.config.pin, {mode: Gpio.INPUT, alert: true});
         this.inputPin.pullUpDown(Gpio.PUD_UP);
@@ -37,14 +45,18 @@ class RpmReader {
         const remap = this.config.remapValues;
         if (!remap) return value;
 
-        if (value < 1000) return remap[0];
-        if (value > 2000) return remap[1];
+        if (value < this.config.minValue) return remap[0];
+        if (value > this.config.maxValue) return remap[1];
 
-        const X_range = 1000;
+        const X_range = this.config.maxValue - this.config.minValue;
         const Y_range = remap[1] - remap[0];
         const XY_ratio = X_range/Y_range
     
-        return ((value - 1000) / XY_ratio + remap[0]);
+        let remaped = ((value - X_range) / XY_ratio + Math.min(...remap));
+
+        if (this.kalmanFilter) remaped = this.kalmanFilter(remaped);
+
+        return remaped;
     }
 }
 
